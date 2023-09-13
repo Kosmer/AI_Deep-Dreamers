d348bc170ef6ff48005ff889d971ec2c05ceb079
Gabri0710 <racitigabriele@gmail.com>
2023-09-13 21:32:39 +0200

    merging added

--- AI/test_utility_script/merge_all.py
+++ AI/test_utility_script/merge_all.py
@@ -16,7 +16,7 @@ for nome_file in os.listdir(cartella_input):
     if nome_file.startswith("ChatGPT") and nome_file.endswith(".txt"):
         # Verifica che il file inizi con "chatgpt" e termini con ".txt"
         percorso_completo_file = os.path.join(cartella_input, nome_file)
-        with open(percorso_completo_file, "r") as file_da_leggere:
+        with open(percorso_completo_file, "r", encoding="utf-8-sig") as file_da_leggere:
             contenuto_da_unire1.append(file_da_leggere.read())
 
 
@@ -24,7 +24,7 @@ for nome_file in os.listdir(cartella_input):
     if nome_file.startswith("Originale") and nome_file.endswith(".txt"):
         # Verifica che il file inizi con "Originale" e termini con ".txt"
         percorso_completo_file = os.path.join(cartella_input, nome_file)
-        with open(percorso_completo_file, "r") as file_da_leggere:
+        with open(percorso_completo_file, "r", encoding="utf-8-sig") as file_da_leggere:
             contenuto_da_unire2.append(file_da_leggere.read())
 
 # Unisci il contenuto dei file in un unico testo
@@ -33,12 +33,12 @@ contenuto_unificato2 = "\n".join(contenuto_da_unire2)
 
 # Scrivi il contenuto unificato nel file di output
 percorso_completo_output1 = os.path.join(cartella_input, file_output1)
-with open(percorso_completo_output1, "w") as file_output1:
+with open(percorso_completo_output1, "w", encoding="utf-8-sig") as file_output1:
     file_output1.write(contenuto_unificato1)
 
 
 percorso_completo_output2 = os.path.join(cartella_input, file_output2)
-with open(percorso_completo_output2, "w") as file_output2:
+with open(percorso_completo_output2, "w", encoding="utf-8-sig") as file_output2:
     file_output2.write(contenuto_unificato2)
 
 print("Unione autori effettuata")
@@ -55,40 +55,40 @@ file_label_poesie_chatgpt = "numero_poesie_ChatGPT.txt"                  #txt co
 file_label_merge = "autori.txt"                                         #txt con le label
 
 
-with open(file_poesie_originali, 'r') as file1:
+with open(file_poesie_originali, 'r', encoding="utf-8-sig") as file1:
     contenuto_poesie_generali = file1.read()
     if contenuto_poesie_generali[-1] != '\n':
         contenuto_poesie_generali += '\n'
 
 
 # Memorizza il contenuto del file delle poesie degli esseri umani. Successivamente scorre il file, quando trova un * scrive uno 0 (label per esseri umani). 
-with open(file_poesie_originali, 'r') as file1:
-    with open(file_label_poesie_originali, 'w') as poesie_originali:
-        with open(file_label_poesie_originali, "r+") as poesie_originali:
+with open(file_poesie_originali, 'r', encoding="utf-8-sig") as file1:
+    with open(file_label_poesie_originali, 'w', encoding="utf-8-sig") as poesie_originali:
+        with open(file_label_poesie_originali, "r+", encoding="utf-8-sig") as poesie_originali:
             for linea in file1:
                 if linea[0]=="*":
                     poesie_originali.write("0\n")
             
 
 # Memorizza le label scritte nel file
-with open(file_label_poesie_originali, "r+") as poesie_originali:
+with open(file_label_poesie_originali, "r+", encoding="utf-8-sig") as poesie_originali:
             label_poesie_originali = poesie_originali.read()
 
 
-with open(file_poesie_chatgpt, 'r') as file2:
+with open(file_poesie_chatgpt, 'r', encoding="utf-8-sig") as file2:
     contenuto_poesie_chatgpt = file2.read()
 
 # Memorizza il contenuto del file delle poesie di chatgpt. Successivamente scorre il file, quando trova un * scrive un 1 (label per chatgpt). 
-with open(file_poesie_chatgpt, 'r') as file2:
-    with open(file_label_poesie_chatgpt, 'w') as poesie_chatgpt:
-        with open(file_label_poesie_chatgpt, "r+") as poesie_chatgpt:
+with open(file_poesie_chatgpt, 'r', encoding="utf-8-sig") as file2:
+    with open(file_label_poesie_chatgpt, 'w', encoding="utf-8-sig") as poesie_chatgpt:
+        with open(file_label_poesie_chatgpt, "r+", encoding="utf-8-sig") as poesie_chatgpt:
             for linea in file2:
                 if linea[0]=="*":
                     poesie_chatgpt.write("1\n")
             
 
 # Memorizza le label scritte nel file
-with open(file_label_poesie_chatgpt, "r+") as poesie_chatgpt:
+with open(file_label_poesie_chatgpt, "r+", encoding="utf-8-sig") as poesie_chatgpt:
             label_poesie_chatgpt = poesie_chatgpt.read()
 
 # Unisce i contenuti dei file delle poesie e dei file delle label
@@ -96,10 +96,10 @@ poesie_unite = contenuto_poesie_generali + contenuto_poesie_chatgpt
 label_unite = label_poesie_originali + label_poesie_chatgpt
 
 # Scrivi il contenuto unito nel file di output
-with open(file_poesie_merge, 'w') as file_output:
+with open(file_poesie_merge, 'w', encoding="utf-8-sig") as file_output:
     file_output.write(poesie_unite)
 
-with open(file_label_merge, 'w') as file_output:
+with open(file_label_merge, 'w', encoding="utf-8-sig") as file_output:
     file_output.write(label_unite)
 
 
